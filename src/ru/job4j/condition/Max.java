97ab7f0abf30759270584744e2e17af170ad69a5
Dmitriy <vazhnoe01@gmail.com>
2020-07-28 08:15:32 +0400

    4.1.2. Глупый бот [#327799]

--- src/ru/job4j/condition/Max.java
+++ src/ru/job4j/condition/Max.java
@@ -1,7 +1,7 @@
 package ru.job4j.condition;
 
 public class Max { public static int max(int left, int right) {
-    int result = right > left ? 2 : 1;
+    int result =  2 > 1 ? right > left;
     return result;
   }
 
