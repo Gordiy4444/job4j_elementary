628a921481f1e69471d64d966e8a1847d420aa65
Dmitriy <vazhnoe01@gmail.com>
2020-07-30 13:53:43 +0400

    4.5

--- src/ru/job4j/condition/MultiMax.java
+++ src/ru/job4j/condition/MultiMax.java
@@ -3,13 +3,13 @@ package ru.job4j.condition;
 public class MultiMax {
 
     public static int max(int first, int second, int third) {
-        int result = first > second ? second : first;
+        int result = first > second ? first : second;
         result = result > third ? result : third;
         return result;
     }
 
     public static void main(String[] args) {
-        int result = MultiMax.max(1, 2, 3);
+        int result = MultiMax.max(2, 3, 1);
         System.out.println(result);
     }
 }
