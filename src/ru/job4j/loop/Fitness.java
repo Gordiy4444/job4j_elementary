d8d0d2d8d6f60fcce5478081ac5ed2e72627143a
Dmitriy <vazhnoe01@gmail.com>
2020-08-01 16:39:24 +0400

    5.4

--- src/ru/job4j/loop/Fitness.java
+++ src/ru/job4j/loop/Fitness.java
@@ -3,17 +3,17 @@ package ru.job4j.loop;
 public class Fitness {
     public static int calc(int ivan, int nik) {
         int month = 0;
-         while (ivan <= nik){
+         while (ivan <= nik) {
              ivan *= 3;
              nik *= 2;
              month++;
+         }
 
-        }
         return month;
     }
 
     public static void main(String[] args) {
-        System.out.println(calc(60, 120 ));
+        System.out.println(calc(60, 120));
 
     }
 }
