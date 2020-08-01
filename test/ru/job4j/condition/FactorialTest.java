c8840099a47cd267caa9a5243e0a2b22d6c29b1c
Dmitriy <vazhnoe01@gmail.com>
2020-08-01 10:36:53 +0400

    5.2

--- test/ru/job4j/condition/FactorialTest.java
+++ test/ru/job4j/condition/FactorialTest.java
@@ -10,14 +10,14 @@ import static org.junit.Assert.assertThat;
 public class FactorialTest {
     @Test
     public void whenCalculateFactorialForFiveThenOneHundreedTwenty() {
-        int result = Counter.calc(0);
+        int result = Factorial .calc(0);
         int expected = 1;
         assertThat(result, is(expected));
     }
 
     @Test
     public void whenCalculateFactorialForZeroThenOne() {
-        int result = Counter.calc(5);
+        int result = Factorial .calc(5);
         int expected = 120;
         assertThat(result, is(expected));
     }
