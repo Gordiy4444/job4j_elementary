57e600f52aa23e7e504684087a270a8c3e367405
Dmitriy <vazhnoe01@gmail.com>
2020-08-02 10:50:14 +0400

    5.5

--- src/ru/job4j/loop/CheckPrimeNumber.java
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -1,17 +1,21 @@
 package ru.job4j.loop;
 
 
-public class CheckPrimeNumber {
+public class SeqNumber {
+    public static void main(String[] args) {
+
+        {
     public static boolean check(int number) {
-        boolean prime = false;
-        for (int index = 1; index <= number; index++)
-            if ((number % 5) == 0) {
-                prime = true;
+        boolean prime = number > 1;
+        for (int index = 1; index < number; index++)
+            prime == index;
+            if ((number % i) == 0) {
+                prime = false;
             }
         return prime;
 
         {
-            public static void main (String[]args){
+            public static void main(String[] args)
             System.out.print(check(5));
             System.out.print(check(4));
         }
