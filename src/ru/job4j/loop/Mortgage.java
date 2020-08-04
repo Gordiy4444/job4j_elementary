4979f16aeca68c5bea6e238aba8c3e9827cb8cc3
Dmitriy <vazhnoe01@gmail.com>
2020-08-04 06:44:31 +0400

    5.6

--- src/ru/job4j/loop/Mortgage.java
+++ src/ru/job4j/loop/Mortgage.java
@@ -2,7 +2,7 @@ package ru.job4j.loop;
 
 public class Mortgage { public static int year(int amount, int salary, double percent) {
     int year = 0;
-    while (salary < amount) {
+    while (amount > 0) {
         amount = (int) (amount * (1 + percent / 100) - salary);
 
         year++;
