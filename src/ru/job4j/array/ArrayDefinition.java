a160b61e49b6be2b4a0e1caaeb2f6fb3f551519e
Gordiy4444 <vazhnoe01@gmail.com>
2020-08-07 19:50:56 +0400

    602

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -5,5 +5,15 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[10];
         float[] prices = new float[10];
+        String[] names = new String[4];
+        names[0] = "Sergey Mayer";
+        names[1] = "Rail Shamsemuhametov";
+        names[2] = "Korobeinikov Stas";
+        names[3] = "Andrei Hincu";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
+
     }
 }
