2b13e0dd02e9504051b20f6207c95ce1224d144a
Gordiy4444 <vazhnoe01@gmail.com>
2020-08-09 14:52:11 +0400

    605

---
+++ src/ru/job4j/array/FindLoop.java
@@ -0,0 +1,21 @@
+package ru.job4j.array;
+
+public class FindLoop {
+        public static int indexOf(int[] data, int el) {
+            int rst = -1;
+            for (int index = 0; index < data.length; index++) {
+                if (data[index] == el) {
+                    rst = index;
+                    break;
+                }
+            }
+            return rst;
+        }
+
+        public static void main(String[] args) {
+            int[] array = indexOf(10, 8);
+            for (int index = 0; index < array.length; index++) {
+                System.out.println(array[index]);
+            }
+        }
+    }
