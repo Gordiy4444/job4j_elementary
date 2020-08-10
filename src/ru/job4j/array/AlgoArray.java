66e4e4cb6a1bca75807beb3e5098f3d9f1ee1876
Gordiy4444 <vazhnoe01@gmail.com>
2020-08-10 10:05:51 +0400

    62

--- src/ru/job4j/array/AlgoArray.java
+++ src/ru/job4j/array/AlgoArray.java
@@ -2,15 +2,15 @@ package ru.job4j.array;
 
 public class AlgoArray {public static void main(String[] args) {
     int[] array = new int[] {5, 3, 2, 1, 4};
-    int bucketone = array[0];
+    int one = array[0];
     array[0] = array[3];
-    array[3] = bucketone;
-    int buckettwo = array[1];
+    array[3] = one;
+    int two = array[1];
     array[1] = array[2];
-    array[2] = buckettwo;
-    int bucketthree = array[3];
+    array[2] = two;
+    int three = array[3];
     array[3] = array[4];
-    array[4] = bucketthree;
+    array[4] = three;
 
 
     for (int index = 0; index < array.length; index++) {
