e4d1e6bf9d23e68845ec36e8adfd9a237835e588
Gordiy4444 <vazhnoe01@gmail.com>
2020-08-13 15:56:55 +0400

    622

--- src/ru/job4j/array/Check.java
+++ src/ru/job4j/array/Check.java
@@ -4,9 +4,19 @@ public class Check {
     public static boolean mono(boolean[] data) {
     boolean result = true;
             for (int index = 0; index < data.length; index++) {
+                if (data[data.length] == 0) {
+                 result = false;
+                }
+
 
          }
     return result;
 }
+    public static void main (String[]args){
+        int[] nums = new int[]{1, 2, 3, 4, 5, 6};
+        int[] rsl = mono(nums);
+        for (int index = 0; index < rsl.length; index++)
+            System.out.println(rsl[index]);
+    }
 }
 
