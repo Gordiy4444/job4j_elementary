68ca9e8c0af56bab0c423356d110f3c1c487f3f8
Gordiy4444 <vazhnoe01@gmail.com>
2020-08-13 12:00:45 +0400

    622

--- src/ru/job4j/array/Turn.java
+++ src/ru/job4j/array/Turn.java
@@ -2,8 +2,8 @@ package ru.job4j.array;
 
 public class Turn {
     public static int[] back(int[] array) {
-        int temp = array[0];
         for (int index = 0; index < array.length/2; index++) {
+            int temp = array[0];
             array[index] = array[array.length - 1 - index];
             array[array.length - 1 - index] = temp;
 
