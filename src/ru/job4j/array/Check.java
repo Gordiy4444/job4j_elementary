a024fcc07d1e602980b82cf29c77e1ee7b1dbf89
Gordiy4444 <vazhnoe01@gmail.com>
2020-08-14 07:18:50 +0400

    622

--- src/ru/job4j/array/Check.java
+++ src/ru/job4j/array/Check.java
@@ -4,8 +4,9 @@ public class Check {
     public static boolean mono(boolean[] data) {
     boolean result = true;
             for (int index = 0; index < data.length; index++) {
-                 { data[index]  = true;
+                 { if (int data[index]  == 0);
                  result = false;
+                 break;
                 }
 
 
