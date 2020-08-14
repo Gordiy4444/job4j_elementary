12f8887c26d49ea92172438498f75a8db046d5a1
Gordiy4444 <vazhnoe01@gmail.com>
2020-08-14 18:25:00 +0400

    6.3

--- src/ru/job4j/array/ArrayChar.java
+++ src/ru/job4j/array/ArrayChar.java
@@ -2,7 +2,23 @@ package ru.job4j.array;
 
 public class ArrayChar {
     public static boolean startsWith(char[] word, char[] pref) {
-    boolean result = true;
-    return result;
-}
+        boolean result = true;
+
+        for (int index = 0; index < word.length; index++) {
+            for (int i = 0; i < pref.length; index++) {
+                if (word[index] != pref[i]) {
+                    result = false;
+                    break;
+                }
+            }
+            return result;
+        }
+
+
+        public static void main (String[]args){
+            boolean[] nums = new boolean[]{};
+            boolean rsl = startsWith();
+            System.out.println(rsl);
+        }
+    }
 }
