cb0c385dbfd1ab24284153187ed3f6d6847eba0e
Gordiy4444 <vazhnoe01@gmail.com>
2020-08-15 10:53:27 +0400

    6.41

---
+++ src/ru/job4j/array/EqLast.java
@@ -0,0 +1,21 @@
+package ru.job4j.array;
+
+public class EqLast {
+    public static boolean check(int[] left, int[] right) {
+        boolean result = true;
+        for (int index = 0; index < left.length; index++) {
+            for (int i = 0; i < right.length; index++)
+                if (left[left.length - 1] != right[left.length]) {
+                    return result = false;
+
+                }
+        }
+
+        public static void main (String[]args){
+            int[] left = {1, 2, 3};
+            int[] right = {7, 6, 3};
+            boolean rsl = EqLast.check(left, right);
+            System.out.println(rsl);
+        }
+    }
+}
