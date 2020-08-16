ef8a9193eba513a4127290aa72aea5466b0346a1
Gordiy4444 <vazhnoe01@gmail.com>
2020-08-16 11:57:46 +0400

    6.41

---
+++ src/ru/job4j/array/MinDiapason.java
@@ -0,0 +1,19 @@
+package ru.job4j.array;
+
+public class MinDiapason {
+    public static int findMin(int[] array, int start, int finish) {
+        int min = array[start];
+        for (int index = start; index <= finish; index++) {
+            if (array[index] < min) {
+                min = array[index];
+            }
+        }
+        return min;
+    }
+
+    public static void main(String[] args) {
+        int i = FindLoop.indexOf(new int[]{5, 4, 3, 2, 6, 7}, 2, 1, 4);
+        System.out.println(i);
+
+    }
+}
