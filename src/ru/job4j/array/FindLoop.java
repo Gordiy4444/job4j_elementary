192c7eb55cdd85d82ac5e42bd39fd03a17435197
Gordiy4444 <vazhnoe01@gmail.com>
2020-08-16 07:31:07 +0400

    6.41

--- src/ru/job4j/array/FindLoop.java
+++ src/ru/job4j/array/FindLoop.java
@@ -11,9 +11,23 @@ public class FindLoop {
             }
             return rst;
         }
+    public static int indexOf(int[] data, int el, int start, int finish) {
+        int rst = -1;
+        for (int index = start; index < finish; index++) {
+            if (data[index] == el) {
+                rst = index;
+                break;
+            }
+        }
+        return rst;
+    }
+
 
     public static void main(String[] args) {
-        int index = FindLoop.indexOf(new int[]{5, 4, 3, 2, 6}, 10 );
+        int index = FindLoop.indexOf(new int[]{5, 4, 3, 2, 6, 7,}, 10);
         System.out.println(index);
+        int i = FindLoop.indexOf(new int[]{5, 4, 3, 2, 6, 7,}, 2, 4, 6);
+        System.out.println(i);
+
     }
     }
