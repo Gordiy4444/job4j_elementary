26481667d138a90c8d663a70610e48dee71c2fa1
Gordiy4444 <vazhnoe01@gmail.com>
2020-08-19 14:52:36 +0400

    6.41

---
+++ src/ru/job4j/loop/PrintNTo0.java
@@ -0,0 +1,11 @@
+package ru.job4j.loop;
+
+
+public class PrintNTo0 {
+        public static void out(int n) {
+            for(index = n; index > 0; index--){
+                System.out.println(index);
+            }
+        }
+    }
+
