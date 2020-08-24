bd10f08c5bd01c8ad05e633998c089ae957e1aef
Gordiy4444 <vazhnoe01@gmail.com>
2020-08-24 13:33:26 +0400

    6.41

---
+++ src/ru/job4j/array/MatrixSum.java
@@ -0,0 +1,26 @@
+package ru.job4j.array;
+
+public class MatrixSum {
+    public static int sum(int[][] array) {
+        int rsl = 0;
+        for (int row = 0; row < array.length; row++) {
+            for (int cell = 0; cell < array.length; cell++) {
+                rsl = row + cell;
+
+            }
+        }
+        return rsl;
+
+    }
+
+
+    public static void main(String[] args) {
+        int[][] array = {
+                {1, 2, 3},
+                {3, 2, 1},
+                {2, 3, 1}
+        };
+        MatrixSum.sum(array);
+
+    }
+}
