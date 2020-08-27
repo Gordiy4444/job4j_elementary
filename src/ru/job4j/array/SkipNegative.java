f72d00ab2b9518d5bfc81dced7ceb1aa22c5dc1d
Gordiy4444 <vazhnoe01@gmail.com>
2020-08-27 11:21:23 +0400

    6.41

--- src/ru/job4j/array/SkipNegative.java
+++ src/ru/job4j/array/SkipNegative.java
@@ -2,17 +2,17 @@ package ru.job4j.array;
 
 public class SkipNegative {
     public static int[][] skip(int[][] array) {
-    for (int row = 0; row < array.length; row++) {
-        for (int cell = 0; cell < array.length; cell++) {
-            int rsl = array[cell][row];
-            if (rsl < 0) {
-                array[row][cell] = 0;
+        for (int row = 0; row < array.length; row++) {
+            for (int cell = 0; cell < array.length; cell++) {
+                int rsl = array[cell][row];
+                if (rsl < 0) {
+                    array[row][cell] = 0;
+                }
             }
         }
-    }
-    return array;
+        return array;
 
-}
+    }
 
 
     public static void main(String[] args) {
@@ -21,10 +21,14 @@ public class SkipNegative {
                 {3, 2, 1},
                 {2, 3, 1}
         };
-        System.out.println(skip(array));
-
+        int[][] rsl = skip(array);
+        for (int row = 0; row < rsl.length; row++) {
+            for (int cell = 0; cell < rsl.length; cell++) {
+                System.out.print(rsl[row][cell]);
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
     }
+
 }
-
-
-
