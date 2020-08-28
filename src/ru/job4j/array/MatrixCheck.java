3934cf42f1a6311e2235e6e22f724f2dfd03768d
Gordiy4444 <vazhnoe01@gmail.com>
2020-08-28 20:07:09 +0400

    6.41

--- src/ru/job4j/array/MatrixCheck.java
+++ src/ru/job4j/array/MatrixCheck.java
@@ -14,14 +14,26 @@ public class MatrixCheck {
 
         return result;
     }
+    public static boolean monoVertical(char[][] board, int column) {
+        boolean result = true;
+        for (int i = 0; i < board.length; column++) {
+            if (board[column][i] != 'X') {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
 
     public static void main(String[] args) {
         char[][] board = {
-                {' ', ' ', ' '},
+                {' ', ' ', 'X'},
                 {'X', 'X', 'X'},
-                {' ', ' ', ' '},
+                {' ', ' ', 'X'},
         };
         boolean rsl = monoHorizontal(board, 0);
         System.out.println(rsl);
+        boolean result = monoVertical(board, 2);
+        System.out.println(result);
     }
 }
