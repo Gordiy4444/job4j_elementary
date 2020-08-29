e38f91a8b277d80307b99bed9622a41ed6d51cd6
Gordiy4444 <vazhnoe01@gmail.com>
2020-08-29 13:35:59 +0400

    6.41

--- src/ru/job4j/array/MatrixCheck.java
+++ src/ru/job4j/array/MatrixCheck.java
@@ -3,7 +3,7 @@ package ru.job4j.array;
 public class MatrixCheck {
     public static boolean monoHorizontal(char[][] board, int row) {
         boolean result = true;
-        for (int index = 0; index < board.length; row++) {
+        for (int index = 0; index < board.length; index++) {
                 if (board[row][index] != 'X'){
 
                     result = false;
@@ -27,11 +27,18 @@ public class MatrixCheck {
         }
         return result;
     }
-
+    public static char[] extractDiagonal(char[][] board) {
+        char[] rsl = new char[board.length];
+        for (int n = 0; n < board.length; n++) {
+            if (board[n][n] =='X')
+            rsl[board.length - 1] = board[n][n];
+        }
+        return rsl;
+    }
 
     public static void main(String[] args) {
         char[][] board = {
-                {' ', ' ', 'X'},
+                {'X', ' ', 'X'},
                 {'X', 'X', 'X'},
                 {' ', ' ', 'X'},
         };
@@ -39,5 +46,7 @@ public class MatrixCheck {
         System.out.println(rsl);
         boolean result = monoVertical(board, 2);
         System.out.println(result);
+        char[] res = extractDiagonal(board);
+        System.out.println(res);
     }
 }
