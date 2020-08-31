37ab421880a05ca41bd640dfd1693b714006d2d8
Gordiy4444 <vazhnoe01@gmail.com>
2020-08-31 17:28:50 +0400

    6.41

---
+++ src/ru/job4j/array/CheckThree.java
@@ -0,0 +1,20 @@
+package ru.job4j.array;
+
+public class CheckThree {
+    public static boolean moon(int[] data) {
+        boolean result = true;
+        for (int index = 1; index < data.length; index++) {
+            if(data[index] < data[index - 1]){
+                result = false;
+                break;
+            }
+
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        boolean index = moon(new int[] {1, 2, 9, 5, 6, 7});
+        System.out.println(index);
+    }
+}
---
+++ src/ru/job4j/array/FindLooped.java
@@ -0,0 +1,21 @@
+package ru.job4j.array;
+
+public class FindLooped {
+    public static int indexOff(int[] data, int[] array ){
+        int rst = -1;
+        for (int index = 0; index < data.length; index++) {
+            for (int i = 0; i < array.length; i++)
+            if (data[index] == array[i]) {
+                rst = index;
+
+            }
+        }
+        return rst;
+    }
+
+    public static void main(String[] args) {
+        int index = FindLooped.indexOff(new int[]{5, 4, 3, 2, 6, 7}, new int[]{8, 9, 31, 5, 7});
+        System.out.println(index);
+    }
+}
+
