b58de164cd95832c45409ab0d77495fe6c985369
Gordiy4444 <vazhnoe01@gmail.com>
2020-09-06 11:59:37 +0400

    6.41

--- src/ru/job4j/condition/Max.java
+++ src/ru/job4j/condition/Max.java
@@ -1,9 +1,12 @@
 package ru.job4j.condition;
 
-public class Max { public static int max(int left, int right) {
-    int result =  right > left ? right : left;
-    return result;
+public class Max {
+    public static int max(int left, int right) {
+    return right > left ? right : left;
   }
+    public static int max(int left, int right,int ahead) {
+        return  right > left > ahead ? right : left : ahead;
+    }
   public static void main(String[] args) {
     int msg = Max.max(6, 5);
     System.out.println(msg);
